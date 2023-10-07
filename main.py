b3017b3878b16fff0ae8cdc10e01f73250c664a0
Rohan Shah <57906961+rshah713@users.noreply.github.com>
2023-10-06 23:36:53 -0400

    add control directions

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ pygame.init()
 screen = pygame.display.set_mode(SCREEN_SIZE)
 clock = pygame.time.Clock()
 font = pygame.font.Font('freesansbold.ttf', 15)
+smaller_font = pygame.font.Font('freesansbold.ttf', 12)
 pygame.display.set_caption('DotVenture!')
 screen.fill(BLACK)
 
@@ -25,24 +26,31 @@ while True:
             platforms = level1.platforms
             target = level1.target
             title = level1.title
-            directions = ""
+            directions = "Arrow keys to move and jump"
+            
         elif level == 2:
             cursor = level2.cursor.copy()
             platforms = level2.platforms
             target = level2.target
             title = level2.title
+            directions = ""
 
         level_complete = False
         level_switch = False
+        
+        directions_text = smaller_font.render(directions, True, WHITE)
+        directions_text_rect = directions_text.get_rect()
+        directions_text_rect.midleft = (0, SCREEN_SIZE[1]//2)
+        
         level_title = "Level {} - {}".format(level, title)
-        level_score = font.render(str(level_title), True, WHITE)
+        level_score = font.render(level_title, True, WHITE)
         level_score_rect = level_score.get_rect()
         level_score_rect.topleft = (0, 0)
         
     for platform in platforms:
         pygame.draw.rect(screen, BLUE, platform)
         
-        
+    screen.blit(directions_text, directions_text_rect)
     screen.blit(level_score, level_score_rect)
     pygame.draw.rect(screen, WHITE, cursor)
     pygame.draw.rect(screen, YELLOW, target)
