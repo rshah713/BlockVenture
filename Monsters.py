68fa8f01739601ad3762c9133f8970b33fc712a6
Rohan Shah <57906961+rshah713@users.noreply.github.com>
2023-11-14 13:11:59 -0500

    add flyer monster

--- Monsters.py
+++ Monsters.py
@@ -2,7 +2,7 @@ import pygame
 from game_data import FPS
 class Monster:
     
-    def __init__(self, m_type='lava', pos=None, size=None, jump_height=None, speed=None, acceleration=None, platform=None):
+    def __init__(self, m_type='lava', pos=None, size=None, jump_height=None, speed=None, acceleration=None, platform=None, range_y=None):
         '''
         m_type: the monster type to create
         pos: (optional), where to position the monster
@@ -11,6 +11,7 @@ class Monster:
         speed: (optional) the speed of the monster in the vertical/horizontal direction
         acceleration: (optional) for both horizontal or vertical
         platform: (tiger), what platform to guard
+        range_y: (flyer), representing the range in the y-direction, assume upward direction
         '''
         self.m_type = m_type
         self.jump_height = jump_height
@@ -19,6 +20,7 @@ class Monster:
         self.pos = pos
         self.size = size
         self.platform = platform
+        self.range = range_y
         self.create_monster()
         
     def create_monster(self):
@@ -35,12 +37,20 @@ class Monster:
             self.color = (255, 165, 0)
             self.pos = (0, 0) if self.pos is None else self.pos
             self.size = (15, 15) if self.size is None else self.size
-            self.speed = 3
+            self.speed = 3 if self.speed is None else self.speed
             self.monster = pygame.Rect(*self.pos, *self.size)
             self.dir = 'right'
             self.center_on_platform(self.platform)
-            
-            
+        elif self.m_type == 'flyer':
+            self.color = (128, 0, 128)
+            self.pos = (0, 0) if self.pos is None else self.pos
+            self.size = (15, 15) if self.size is None else self.size
+            self.speed = 4 if self.speed is None else self.speed
+            self.monster = pygame.Rect(*self.pos, *self.size)
+            self.dir = 'up'
+
+
+
     def get_monster(self):
         return self.monster
     
@@ -69,6 +79,13 @@ class Monster:
                 self.monster.x -= self.speed
             if not(self.platform.x <= self.monster.x <= self.platform.right):
                 self.dir = 'right' if self.dir == 'left' else 'left'
+        elif self.m_type == 'flyer':
+            if self.dir == 'up':
+                self.monster.y -= self.speed
+            elif self.dir == 'down':
+                self.monster.y += self.speed
+            if not(self.pos[1] <= self.monster.y <= self.pos[1]+self.range):
+                self.dir = 'up' if self.dir == 'down' else 'down'
                 
     
         
--- levels/level5.py
+++ levels/level5.py
@@ -4,7 +4,7 @@ from game_data import SCREEN_SIZE
 from Monsters import Monster
 
 platforms = [
-    pygame.Rect(185, 255, 85, 5),
+    pygame.Rect(160, 255, 85, 5),
     pygame.Rect(280, 280, 85, 5),
     pygame.Rect(400, 255, 85, 5)
 ]
@@ -13,6 +13,6 @@ cursor = start_on_platform(platforms[0])
 
 target = pygame.Rect(265, 125, 20, 20)
 
-monsters = []
+monsters = [Monster('flyer', pos=(260,130), range_y=170)]
 
 title = "Uncharted Territory"
