213e35f99bcc4a0a0a54d37bb749b22fac299b72
Rohan Shah <57906961+rshah713@users.noreply.github.com>
2024-01-27 12:34:37 -0500

    add custom fonts and tweak jump speed

--- levels/level7.py
+++ levels/level7.py
@@ -3,27 +3,19 @@ from utils import start_on_platform
 from game_data import SCREEN_SIZE
 from Monsters import Monster
 
-platforms = [
-    pygame.Rect(265, 285, 85, 5),
+platforms = [ 
     pygame.Rect(280, 250, 85, 5),
-    pygame.Rect(415, 230, 85, 5),
-    pygame.Rect(230, 280, 145, 5)
+    pygame.Rect(365, 190, 85, 5),
+    pygame.Rect(410, 240, 85, 5)
 ]
-green_platforms = [
-    pygame.Rect(415, 250, 85, 5),
-    pygame.Rect(230, 280, 145, 5),
-    pygame.Rect(110, 280, 85, 5),
-    pygame.Rect(110, 230, 85, 5),
-    pygame.Rect(110, 180, 85, 5),
-    pygame.Rect(110, 130, 85, 5)
-]
+green_platforms = []
 
 cursor = start_on_platform(platforms[0])
 sword = pygame.Rect(0,0,0,0)
 
 target = pygame.Rect(130, 70, 20, 20)
-platform_switch = pygame.Rect(455, 200, 20, 20)
+platform_switch = pygame.Rect(405, 175, 20, 20)
 
-monsters = [Monster('tiger', platform=platforms[3])]
+monsters = [Monster('lava').center_on_platform(platforms[1])]
 
 title = "It's dangerous to go alone"
